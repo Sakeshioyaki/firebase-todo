1abb7187859ff51a261fa6ad5524d3459650961d
EC2 Default User <ec2-user@ip-172-31-17-49.ec2.internal>
2021-05-21 10:05:37 +0000

    User firebaseg

---
+++ src/lib/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+const firebaseConfig = {
+    apiKey: "AIzaSyCbqCEDbi6YlKfr1ZQV6UL260_gjEyCyEA",
+    authDomain: "rin-firebase-cf66e.firebaseapp.com",
+    projectId: "rin-firebase-cf66e",
+    storageBucket: "rin-firebase-cf66e.appspot.com",
+    messagingSenderId: "394185251671",
+    appId: "1:394185251671:web:ef7dc8bd76323088947c12"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
